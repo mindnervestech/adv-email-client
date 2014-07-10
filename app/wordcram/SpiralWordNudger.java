817c6c7253189f5d18bd4304d09b4f49ea8083b2
mindnervestech <jagbir.paul@gmail.com>
2014-07-10 11:39:13 +0530

    more hack 3

--- app/wordcram/SpiralWordNudger.java
+++ app/wordcram/SpiralWordNudger.java
@@ -1,6 +1,5 @@
 package wordcram;
 
-import processing.core.PApplet;
 import processing.core.PVector;
 
 public class SpiralWordNudger implements WordNudger {
@@ -14,17 +13,37 @@ public class SpiralWordNudger implements WordNudger {
 
         thetaIncrement = powerMap(1, attempt, 0, 600, 0.5f, 0.3f);
         float theta = thetaIncrement * attempt;
-        float x = PApplet.cos(theta) * rad;
-        float y = PApplet.sin(theta) * rad;
+        float x = cos(theta) * rad;
+        float y = sin(theta) * rad;
         return new PVector(x, y);
     }
+    
+    static public final float cos(float angle) {
+        return (float)Math.cos(angle);
+      }
+    
+    static public final float sin(float angle) {
+        return (float)Math.sin(angle);
+      }
 
     private float powerMap(float power, float v, float min1, float max1,
             float min2, float max2) {
 
-        float val = PApplet.norm(v, min1, max1);
-        val = PApplet.pow(val, power);
-        return PApplet.lerp(min2, max2, val);
+        float val = norm(v, min1, max1);
+        val = pow(val, power);
+        return lerp(min2, max2, val);
     }
+    
+    static public final float lerp(float start, float stop, float amt) {
+        return start + (stop-start) * amt;
+      }
+    
+    static public final float norm(float value, float start, float stop) {
+        return (value - start) / (stop - start);
+      }
+    
+    static public final float pow(float n, float e) {
+        return (float)Math.pow(n, e);
+      }
 
 }
