8c15634b8987b0dda1097262a59b65527da5aee0
mindnervestech <mindnervestech@gmail.com>
2015-01-06 18:57:24 +0530

    change

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -16,7 +16,6 @@ angular.module('email-client', [
   'ngSanitize',
   'angularTreeview',
   'ngDragDrop',
-  'angularjs-dropdown-multiselect',
   'angularFileUpload'
 ])
   .config(function ($routeProvider) {
